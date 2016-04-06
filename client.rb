4415b19a79ce6e396b55d5468c3c047af715b38d
kyatul <atul.21j@gmail.com>
2016-04-06 11:24:38 +0530

    Initial client-server code

---
+++ client.rb
@@ -0,0 +1,12 @@
+# Client side program to access a server
+require 'socket'
+
+hostname = 'localhost'
+port = 2000
+
+s = TCPSocket.open(hostname, port)
+
+while line = s.gets
+  puts line.chop
+end
+s.close
---
+++ server.rb
@@ -0,0 +1,10 @@
+# Server side program for accepting a request
+require 'socket'
+
+server = TCPServer.open(2000)
+loop {
+  client = server.accept
+  client.puts(Time.now.ctime)
+  client.puts('Closing the connection')
+  client.close
+}
